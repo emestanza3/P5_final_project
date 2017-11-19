32598f41b1314e609cc7dee123ee5488e4a1e398
emestanza3 <eduardomestanza_96@gatech.edu>
2017-11-19 16:56:42 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+var svg = d3.select('svg');
+
+var svgWidth = +svg.attr('width');
+var svgHegith = +svg.attr('hegiht');
+
+var padding = {t: 40, r: 40, b: 40, l: 40};
 
 // Global dataset
 var movies;
